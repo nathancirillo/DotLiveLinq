fc0c25db31f5e0fde2beb42e64c8af21158fac3d
nathancirillo <cirillo.nathan@gmail.com>
2023-06-20 22:13:41 -0300

    Other examples
    Created examples using agregation, projection and others.

--- Persistence/WorkDbContext.cs
+++ Persistence/WorkDbContext.cs
@@ -14,8 +14,8 @@ namespace DotLive.Persistence
                 new Employee(3, "Thalles","Gestão",15_000m, new List<Invoice>{new Invoice(3,"06/2023",3,15_000m)}),
                 new Employee(4, "Jaqueline","RH",2_530m, new List<Invoice>{new Invoice(4,"06/2023",4,2_530m)}),
                 new Employee(5, "Fernanda","Support",3_892m, new List<Invoice>{new Invoice(5,"06/2023",5,3_892m)}),
-                new Employee(6, "Luana","Sales",14_243m, new List<Invoice>{new Invoice(6,"06/2023",6,14_243m)}),
-                new Employee(7, "Priscila", "TI", 8_240m, new List<Invoice>{new Invoice(7,"06/2023",7,8_240m)}),
+                new Employee(6, "Luana","Sales",14_243m, new List<Invoice>{new Invoice(6,"06/2023",6,14_243m), new Invoice(7,"05/2023",6,14_243m)}),
+                new Employee(7, "Priscila", "TI", 8_240m, new List<Invoice>{new Invoice(8,"06/2023",7,8_240m)}),
                 new Employee(8, "Matheus", "TI", 10_000m,new List<Invoice>{}),
                 new Employee(9, "Bruna", "Atendimento", 1_785m,new List<Invoice>{new Invoice(9,"06/2023",9,1_785m)}),
                 new Employee(10, "Lucas","TI",10_000m, new List<Invoice>{new Invoice(10,"06/2023",10,10_000m)})
--- Program.cs
+++ Program.cs
@@ -38,12 +38,12 @@ Employee jonas = dbEmployees.FirstOrDefault(e => e.FullName == "Jonas");
 // Um outro exemplo seria pegar os funcionários e trazer somente o seu nome. 
 // Podemos usar a projeção com instanciação também (ultimo exemplo). 
 List<string> nomes = dbEmployees.Select(e => e.FullName).ToList();
-List<EmployeeViewModel> employeesViewModel = dbEmployees.Select(e => new EmployeeViewModel(e.FullName, e.BusinessArea,e.Salary)).ToList();
+List<EmployeeViewModel> employeesViewModel = dbEmployees.Select(e => new EmployeeViewModel(e.FullName, e.BusinessArea, e.Salary)).ToList();
 
 
 // Também temos o método SelectMany para projeção. Permite criar uma lista com base em elementos/propriedades que são listas. 
 // Permite fazer operações nas listagens das listagens. Ou seja, nas propriedades do tipo coleção que estão em uma lista de objetos.  
-List<Invoice> allInvoices = dbEmployees.SelectMany(e => e.Invoices).ToList(); 
+List<Invoice> allInvoices = dbEmployees.SelectMany(e => e.Invoices).ToList();
 
 
 // ORDENAÇÃO: 
@@ -52,7 +52,7 @@ List<Employee> employeesSalaryAsc = dbEmployees.OrderBy(e => e.Salary).ToList();
 // Ordenando salário em ordem decrescente
 List<Employee> employeesSalaryDesc = dbEmployees.OrderByDescending(e => e.Salary).ToList();
 // Ordenando por setor e depois por salário decrescente
-List<Employee> employeesOrderByAreaThenBySalaryDesc = 
+List<Employee> employeesOrderByAreaThenBySalaryDesc =
 dbEmployees.OrderBy(e => e.BusinessArea)
 .ThenByDescending(e => e.Salary).ToList();
 
@@ -62,16 +62,98 @@ dbEmployees.OrderBy(e => e.BusinessArea)
 // Sempre que realizamos o agrupamento, a propriedade que estamos agrupando é a chave.
 
 // Imagine querer a quantidade de funcionario de cada setor. 
-var employeesCountByBusiness = 
+var employeesCountByBusiness =
 dbEmployees.GroupBy(e => e.BusinessArea)
-.Select(g => new {Department = g.Key, Count = g.Count()});
+.Select(g => new { Department = g.Key, Count = g.Count() });
 
 //Agora quero a quantidade de pessoas que ganham acima de 10K por área. 
-var employeesOver10_000CountByArea = 
+var employeesOver10_000CountByArea =
 dbEmployees.GroupBy(e => e.BusinessArea)
-.Select(g => new {Department = g.Key, Count = g.Count( e => e.Salary >= 10_000m)});
+.Select(g => new { Department = g.Key, Count = g.Count(e => e.Salary >= 10_000m) });
 
 
+//Quantidade de funcionários com recibos emitidos 
+var amoutOfInvoices = dbEmployees.GroupBy(e => e.Invoices.Count)
+                                 .Select(g => new { NumberInvoices = g.Key, Employees = g.Select(e => e.FullName) })
+                                 .ToList();
+
+
+// JUNÇÃO (JOINS)
+// A ideia será mostrar como funciona o inner join e o left join.
+
+//INNER JOIN 
+var innerJoin = dbEmployees.Join(allInvoices,
+    employee => employee.Id, invoice => invoice.EmployeeId,
+    (employee, invoice) => new { Name = employee.FullName, InvoiceAmount = invoice.Amount, InvoiceId = invoice.Id }).ToList();
+
+var innerJoinQuerySintax = from employee in dbEmployees
+                           join invoice in allInvoices
+                           on employee.Id equals invoice.EmployeeId
+                           select new
+                           {
+                               Name = employee.FullName,
+                               InvoiceAmount = invoice.Amount,
+                               InvoiceId = invoice.Id
+                           };
+var innerJoinQuerySintaxResult = innerJoinQuerySintax.ToList();
+
+
+//LEFT JOIN
+var leftJoin = dbEmployees.GroupJoin(allInvoices,
+    employee => employee.Id, invoice => invoice.EmployeeId,
+    (employee, invoices) => new
+    {
+        Name = employee.FullName,
+        Invoices = invoices.ToList()
+    }).ToList();
+
+
+
+var leftJoinQuerySintax = from employee in dbEmployees
+                          join invoice in allInvoices
+                          on employee.Id equals invoice.EmployeeId into tempInvoices
+                          select new 
+                          {
+                            Name = employee.FullName,
+                            Invoices = tempInvoices.ToList()
+                          };
+var leftJoinQuerySintaxResult = leftJoinQuerySintax.ToList();
+
+
+//PARTIÇÃO 
+//consiste em pegar partes, por exemplo, a paginação. 
+//existem dois métodos: skip e take 
+
+//buscando somente três funcionários na ordem que está cadastrado
+var threeEmployees = dbEmployees.Take(3).ToList();
+
+//buscando os top 5 salários do empregados 
+var topFiveSalaries = employeesSalaryDesc.Take(5).ToList();
+
+//pulando os top 5 salários e pegando o restante
+var allBut5TopSalaries = employeesSalaryDesc.Skip(5).ToList();
+
+//aplicando paginação (exemplo):
+var top5SalariesPaginated = Paginate(1,3);
+
+IEnumerable<Employee> Paginate(int page, int pageSize)
+{
+    return dbEmployees.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+}
+
+
+//AGREGAÇÃO 
+// consiste em usar métodos que fazem agregação de informações, como por exemplo: calcular total, minímo, máximo, média, etc. 
+var totalSalaries = dbEmployees.Sum(e => e.Salary);  
+var averageSalaries = dbEmployees.Average(e => e.Salary);
+var maxSalary = dbEmployees.Max(e => e.Salary); 
+var minSalary = dbEmployees.Min(e => e.Salary);
+
+//agora imagine que eu precise calcular a média salarial por setor. 
+//estou misturando agrupamento, projeção e agregação.  
+var averageSalaryByArea = dbEmployees.GroupBy(e => e.BusinessArea)
+                                     .Select(g => new {Area = g.Key, AverageSalary = g.Average(e => e.Salary)})
+                                     .ToList();
 
 
 Console.ReadLine();
